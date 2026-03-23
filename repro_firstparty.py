41448d27458870a4d6c60e1574ebca8446d20f8a
Alex Waygood <alex.waygood@gmail.com>
2026-03-23 15:08:27 +0000

    reproduce the panic

--- repro_firstparty.py
+++ repro_firstparty.py
@@ -8,8 +8,15 @@ from typing import (
 import arrlib as np
 import idxlib as pd
 
+left = cast(pd.Index[bool], ...)
+
 b = np.array([True, False, True], np.bool_)
+s = np.array([np.datetime64(f"2025-10-{day:02d}") for day in (1, 2, 3)], np.datetime64)
+d = np.array([np.timedelta64(second + 1, "s") for second in range(3)], np.timedelta64)
 
-def test(left: pd.Index[bool]):
-    reveal_type(left * b, pd.Index[bool])
+def test(left: pd.Index[bool], b: np.array([True, False, False]))
+assert_type(left * b, pd.Index[bool])
 
+if TYPE_CHECKING:
+    assert_type(left * s, Never)
+    assert_type(left * d, Never)
